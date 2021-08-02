e10d49493aa9845b66b379f43bdc28306b48a80c
Edimar Silva <edimar.silva@lsbrasilia.com.br>
2021-08-02 11:39:57 -0300

    testes com css

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,28 @@ import React from "react";
 import ReactTV from "react-tv";
 import { withNavigation, withFocusable } from "react-tv-navigation";
 
+
 import Main from "./views/template/Main.jsx"
 import Logo from "./views//template/Logo.jsx"
-import Nav  from "./views/template/Nav.jsx"
+import Nav from "./views/template/Nav.jsx"
 
 class App extends React.Component {
   render() {
     return (
-      <div className="app">
-        {/* <Logo/> */}
-        <Main/> 
-        {/* <Nav/>      */}
-      </div>
+      // <div className="app">
+      //   <Logo/>
+      //   <Main/> 
+      //   <Nav/>     
+
+
+      // </div>
+     <div className="container mx-auto   ">
+       texto
+     </div>
     );
   }
 }
 
 const AppWithNavigation = withNavigation(App)
 
-ReactTV.render(<AppWithNavigation/>, document.querySelector('#root'));
+ReactTV.render(<AppWithNavigation />, document.querySelector('#root'));
